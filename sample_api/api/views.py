2c92c54c284600b7b59f971b1198b5b76910aab4
hirontan <hirontan@sora.flights>
2021-08-20 20:30:10 +0900

    [Update] Viewのアップデート

--- sample_api/api/views.py
+++ sample_api/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Task, Tag
+from rest_framework import serializers, viewsets
+from .serializers import TaskSerializer, TagSerializer
 
-# Create your views here.
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
