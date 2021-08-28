23d4a028d1a6d7c2578730b5d52340093681a23a
hirontan <hirontan@sora.flights>
2021-08-28 11:28:29 +0900

    [Update] StateProvider

--- react-query/src/App.tsx
+++ react-query/src/App.tsx
@@ -2,20 +2,23 @@ import { VFC } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ClassicalFetchA } from './components/ClassicalFetchA';
 import { ClassicalFetchB } from './components/ClassicalFetchB';
+import { StateProvider } from './context/StateProvider';
 
 import { Layout } from './components/Layout';
 
 const App: VFC = () => {
   return <BrowserRouter>
     <Layout>
-      <Switch>
-        <Route exact path="fetch-a">
-          <ClassicalFetchA />
-        </Route>
-        <Route exact path="fetch-b">
-          <ClassicalFetchB />
-        </Route>
-      </Switch>
+      <StateProvider>
+        <Switch>
+          <Route exact path="fetch-a">
+            <ClassicalFetchA />
+          </Route>
+          <Route exact path="fetch-b">
+            <ClassicalFetchB />
+          </Route>
+        </Switch>
+      </StateProvider>
     </Layout>
   </BrowserRouter>
 
