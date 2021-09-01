4ac8da68c49e3ba499ee365601f740b05df5efb5
hirontan <hirontan@sora.flights>
2021-09-01 15:17:53 +0900

    [Update] main-rtkitのルーティング設定

--- react-query/src/App.tsx
+++ react-query/src/App.tsx
@@ -9,6 +9,7 @@ import { Layout } from './components/Layout'
 import { ReactQueryA } from './components/ReactQueryA'
 import { ReactQueryB } from './components/ReactQueryB'
 import { MainContext } from './components/MainContext'
+import { MainRTKit } from './components/MainRTKit'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -40,6 +41,9 @@ const App: VFC = () => {
               </Route>
               <Route exact path="/main-context">
                 <MainContext />
+              </Route>
+              <Route exact path="/main-rtkit">
+                <MainRTKit />
               </Route>
             </Switch>
           </Layout>
