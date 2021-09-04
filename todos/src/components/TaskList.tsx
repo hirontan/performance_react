50543234cbbe5abfa66c3d7972c176c44d1d470d
hirontan <hirontan@sora.flights>
2021-09-04 11:11:50 +0900

    [Update] コメント

--- todos/src/components/TaskList.tsx
+++ todos/src/components/TaskList.tsx
@@ -2,6 +2,7 @@ import { VFC, memo } from 'react'
 import { useQueryTasks } from '../hooks/useQueryTasks'
 import { TaskItemMemo } from './TaskItem'
 
+// 一番上のデータが削除された時は、残りの情報がシフトされるので、再レンダリングが走る
 const TaskList: VFC = () => {
   const { status, data } = useQueryTasks()
   console.log('rendered TaskList')
