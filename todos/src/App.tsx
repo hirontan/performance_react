23ea761674212e6eea3d266bc06a27ea573d5428
hirontan <hirontan@sora.flights>
2021-09-04 10:36:20 +0900

    [Update] QueryClientProvider/BrowserRouter/ReactQueryDevtools

--- todos/src/App.tsx
+++ todos/src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import {VFC} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
-function App() {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+const App: VFC = () => {
   return (
-    <div className="">
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 }
